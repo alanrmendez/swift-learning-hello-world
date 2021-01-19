53dabcecc2b7a7d7e80af639241a3d0b43837442
alanrmendez <alan.mendez07@gmail.com>
2021-01-19 13:52:41 -0500

    Commit local project to GitHub repository.

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -2,16 +2,26 @@
 //  ViewController.swift
 //  Hello World
 //
-//  Created by Alan Reynaldo Mendez on 1/8/21.
-//
+//  Created by Alan Mendez on 1/8/21.
+//  Copyright (c) 2021 Aurora. All rights reserved.
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    
+        label.text = textField.text
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
 
